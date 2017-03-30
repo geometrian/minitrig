c49a87983b54589f4af7d3f052388e617cd041e8
imallett <ian@geometrian.com>
2017-03-30 01:39:35 -0600

    Add arbitrary precision to accuracy test and add cache since it's expensive to compute.

--- test-accuracy-msvc-cmath/main.cpp
+++ test-accuracy-msvc-cmath/main.cpp
@@ -2,20 +2,44 @@
 #include <cmath>
 #include <cstdio>
 
-#include "../libminitrig/include.hpp"
+#include <string>
+#include <vector>
+
+#include "../libminitrig/include-f32.hpp"
+#include "../libminitrig/include-highprec.hpp"
 
 
 static void test( char const* name, float(*fn_mini)(float), float(*fn_gt)(float), float low,float high, size_t steps) {
 	assert(steps>0);
 	assert(high>=low);
+
+	std::vector<float> xs(steps+1);
+	for (size_t i=0;i<=steps;++i) {
+		xs[i] = ((float)(i)/(float)(steps))*(high-low) + low;
+	}
+
+	std::vector<float> gts(steps+1);
+	std::string filename = "cache/"+std::string(name)+"_"+std::to_string(steps+1)+"_"+std::to_string(low)+"_"+std::to_string(high)+".f32";
+	FILE* file = fopen(filename.c_str(),"rb");
+	if (file==nullptr) {
+		for (size_t i=0;i<=steps;++i) {
+			gts[i] = fn_gt(xs[i]);
+		}
+		file = fopen(filename.c_str(),"wb");
+		fwrite(gts.data(), sizeof(float),steps+1, file);
+		fclose(file);
+		file = fopen(filename.c_str(),"rb");
+	}
+	fread(gts.data(), sizeof(float),steps+1, file);
+	fclose(file);
+
 	float max_err = 0.0f;
 	float at_x;
 	for (size_t i=0;i<=steps;++i) {
-		float x = ((float)(i)/(float)(steps))*(high-low) + low;
-		float err = fabsf( fn_gt(x) - fn_mini(x) );
+		float err = fabsf( gts[i] - fn_mini(xs[i]) );
 		if (err>max_err) {
 			max_err = err;
-			at_x = x;
+			at_x = xs[i];
 		}
 	}
 	printf("Max err %s in range [%f,%f]: %e (at x=%f)\n",name,(double)(low),(double)(high),(double)(max_err),(double)at_x);
@@ -50,11 +74,11 @@ static void graph( float(*fn)(float), float low,float high, size_t height=11) {
 
 int main(int /*argc*/, char* /*argv*/[]) {
 	#define PI 3.14159265358979323f
-	#define N 100000
-	test( "cos", minitrig::cos, cosf, -1.0f*PI,1.0f*PI, N);
-	test( "sin", minitrig::sin, sinf, -1.0f*PI,1.0f*PI, N);
-	test( "arccos", minitrig::arccos, acosf, -1.0f,1.0f, N);
-	test( "arcsin", minitrig::arcsin, asinf, -1.0f,1.0f, N);
+	#define N 10000
+	test( "cos", minitrig::cos, highprec::cos, -1.0f*PI,1.0f*PI, N);
+	test( "sin", minitrig::sin, highprec::sin, -1.0f*PI,1.0f*PI, N);
+	test( "arccos", minitrig::arccos, highprec::arccos, -1.0f,1.0f, N);
+	test( "arcsin", minitrig::arcsin, highprec::arcsin, -1.0f,1.0f, N);
 
 	printf("%f %f %f %f %f\n",minitrig::sin(0),minitrig::sin(0.5f*PI),minitrig::sin(PI),minitrig::sin(1.5f*PI),minitrig::sin(2.0f*PI));
 
